e5630438f8b9a7c3c440c076bbedacb19747842f
David Egerton <david.egertonsmith@gmail.com>
2020-11-25 21:45:10 +0000

    added event show tidy and new privacy agreement page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'events#index'
+  get 'pages/privacy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :events do
     resources :event_users, only: [:new, :create]
